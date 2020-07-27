27735c053f7660d924231dae8650643c5ddc4fb4
SBN <amdadulislamcl@gmail.com>
2020-07-28 03:39:54 +0600

    add new route

--- routes/web.php
+++ routes/web.php
@@ -14,33 +14,3 @@ Route::group(['prefix'=>'/admin'], function() {
 
 Route::get('/', ['as'=>'home', 'uses'=>'HomeController@index']);
 Route::get('/home', ['as'=>'home', 'uses'=>'HomeController@index']);
-
-//Route::get('/', function () {
-//    return view('users/home');
-//});
-//Route::get('/r', function () {
-//    return view('admin/pages/registration');
-//});
-//
-
-//
-//Route::get('/dd', function () {
-//    return view('admin/layout/layout');
-//});
-//
-//Route::get('/all-user', function () {
-//    return view('admin/pages/all-user');
-//});
-//
-//Route::get('/all-product', function () {
-//    return view('admin/pages/all-product');
-//});
-//
-//Route::get('/product_entry', function () {
-//    return view('admin/pages/Product_Entry');
-//});
-//
-//Route::get('/product-update', function () {
-//    return view('admin/pages/Product-update');
-//});
-
